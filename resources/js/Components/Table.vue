b96f19677b3d2857bbab364731685b8696334e5c
Yosiak-alv <Josias_romero60@outlook.com>
2023-06-07 19:30:55 -0600

    Create Table.vue

---
+++ resources/js/Components/Table.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+<template>
+
+    <div class="bg-grayC-400 relative overflow-x-auto shadow-xl sm:rounded-xl shadow-grayC-500">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-900 dark:text-gray-400">
+                <tr class="">
+                   <slot name="heading"/>
+                </tr>
+            </thead>
+            <tbody>
+                <slot/>
+            </tbody>
+        </table>
+    </div>
+
+</template>
