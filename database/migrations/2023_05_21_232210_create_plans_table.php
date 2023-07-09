3e1110cc820c287209b29eacfa3c619c7343a14c
Yosiak-alv <Josias_romero60@outlook.com>
2023-07-08 22:34:05 -0600

    Update 2023_05_21_232210_create_plans_table.php

--- database/migrations/2023_05_21_232210_create_plans_table.php
+++ database/migrations/2023_05_21_232210_create_plans_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('plans', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->double('price_per_month');
+            $table->double('price');
         });
     }
 
