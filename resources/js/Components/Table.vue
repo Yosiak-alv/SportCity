5a819125802049db6bac7f60a8aa6996cade871a
Yosiak-alv <Josias_romero60@outlook.com>
2023-07-21 22:08:53 -0600

    Update Table.vue

--- resources/js/Components/Table.vue
+++ resources/js/Components/Table.vue
@@ -1,9 +1,18 @@
-<script setup></script>
+<script setup>
+
+defineProps({
+    id:{
+        type:String,
+        required:false,
+        default:''
+    }
+});
+</script>
 <template>
 
     <div class="bg-grayC-400 relative overflow-x-auto shadow-xl sm:rounded-xl shadow-grayC-500">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-900 dark:text-gray-400">
+        <table :id="id" class="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr class="">
                    <slot name="heading"/>
                 </tr>
