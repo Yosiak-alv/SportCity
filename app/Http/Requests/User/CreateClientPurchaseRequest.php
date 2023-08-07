39269f8c662e6b10b95c7e24bdd1b4f6eb6b2bda
Yosiak-alv <Josias_romero60@outlook.com>
2023-08-07 09:02:09 -0600

    Create CreateClientPurchaseRequest.php

---
+++ app/Http/Requests/User/CreateClientPurchaseRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+class CreateClientPurchaseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'product_id' => 'required|array',
+            'product_id.*' =>'numeric|gt:0|decimal:0|max:255', //representa la iteracion de cada una 
+            'quantity' => 'array',
+            'quantity*' => 'numeric|gt:0|decimal:0',
+            'transaction' => ['required', 'string', 'in:Cash,Card',Rule::excludeIf($this->route('suscription') != null)],
+        ];
+    }
+    public function validatedProductIds()
+    {
+        return collect($this->only('product_id')['product_id']);
+    }
+
+    public function quantities(): array
+    {
+        return $this->only('quantity')['quantity'];
+    }
+}
