5ea8cb4b3803221802daa868d41eba1fa67f23ff
Yosiak-alv <Josias_romero60@outlook.com>
2023-08-07 09:03:55 -0600

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -25,5 +26,9 @@ class Product extends Model
     public function purchaseItems():HasMany
     {
         return $this->hasMany(PurchaseItems::class,'purchase_id');
+    }
+    public function gym(): BelongsTo
+    {
+        return $this->belongsTo(Gym::class);
     }
 }
