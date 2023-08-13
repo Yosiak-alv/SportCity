cf50fafff7acb01bbbfe34fce78dc265d78e56f4
Yosiak-alv <Josias_romero60@outlook.com>
2023-08-13 13:47:43 -0600

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -22,6 +23,30 @@ class Product extends Model
     {
         return ['uuid'];
     }
+    // entender que es !!
+    // los items deleted devuelven un not found con y sin softdeletes
+    //pero este metodo ayuda a que en soft deletes se pueda acceder a el como si 
+    // no estuviera eliminado
+    public function resolveRouteBinding($value, $field = null)
+    {
+        return $this->where($field ?? 'id', $value)->withTrashed()->firstOrFail();
+    }
+    public function scopeFilter($query , array $filters)
+    {
+        $query->when($filters['search'] ?? false, function( $query, $search){
+            $query->where(fn($query) =>
+                $query->where('name','like','%'.$search.'%')
+                    ->orWhere('unit_price','like','%'.$search.'%')
+                    ->orWhere('quantity','like','%'.$search.'%')
+            );
+        })->when($filters['trashed'] ?? null, function ($query, $trashed) {
+            if ($trashed === 'with') {
+                $query->withTrashed();
+            } elseif ($trashed === 'only') {
+                $query->onlyTrashed();
+            }
+        });
+    }
 
     public function purchaseItems():HasMany
     {
@@ -30,5 +55,15 @@ class Product extends Model
     public function gym(): BelongsTo
     {
         return $this->belongsTo(Gym::class);
+    }
+
+    //ATTRIBUTES
+    public function getCreatedAtAttribute($created_at)
+    {
+        return Carbon::parse($created_at)->diffForHumans();
+    }
+    public function getUpdatedAtAttribute($updated_at)
+    {
+        return Carbon::parse($updated_at)->diffForHumans();
     }
 }
