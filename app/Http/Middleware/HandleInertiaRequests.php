43ee83c7191eadd1394ff80d684fcd2e86a27d3d
Yosiak-alv <Josias_romero60@outlook.com>
2023-11-09 00:54:48 -0600

    Update HandleInertiaRequests.php

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -33,7 +33,7 @@ class HandleInertiaRequests extends Middleware
 
         return array_merge(parent::share($request), [
             'auth' => [
-                'user' => $request->user()?->makeHidden(['remember_token'])->load('gym'),
+                'user' => $request->user()?->makeHidden(['remember_token'])->load('gym:id,name,address'),
                 'user_role_permissions' => $request?->user()?->getPermissionsViaRoles()->pluck('name'),
                 'user_roles' => $request?->user()?->getRoleNames()
             ],
